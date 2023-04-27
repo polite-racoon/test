7ce4c15c9a264af82b844072401ddd541b9e6eda
galiardi <66950896+galiardi@users.noreply.github.com>
2023-04-27 10:25:54 -0400

    Add Sacramento google font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ import Document, {
   Head,
   Main,
   NextScript,
-} from "next/document";
+} from 'next/document';
 
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
@@ -17,10 +17,16 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com"></link>
           <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="true"
+          ></link>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Sacramento&display=swap"
             rel="stylesheet"
-            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-          />
+          ></link>
         </Head>
         <body>
           <Main />

089275f487e920d8afacffc59fd90940dd44ee57
galiardi <66950896+galiardi@users.noreply.github.com>
2023-07-29 08:28:43 -0400

    pwa working

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -28,6 +28,21 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Sacramento&display=swap"
             rel="stylesheet"
           ></link>
+          <link rel="manifest" href="/manifest.json" />
+          {/* <link
+          href="/icons/favicon-16x16.png"
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+        />
+        <link
+          href="/icons/favicon-32x32.png"
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+        />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#317EFB" /> */}
         </Head>
         <body>
           <Main />
